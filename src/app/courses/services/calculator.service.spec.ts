6372c7dc8bbf233df96d223c1579f386fb0435df
KateGordin <kate.gordin@gmail.com>
2023-11-29 15:54:15 +0100

    Use TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,16 +1,25 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
 
   let calculator: CalculatorService,
       loggerSpy: any;
 
+
   beforeEach(() => {
     console.log("Calling beforeEach");
     loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
 
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    });
+
+    calculator = TestBed.get(CalculatorService);
   });
 
   it('should add two numbers', () => {
