14552d637ff872c341670c5c2adcfac1e6b6e6e6
KateGordin <kate.gordin@gmail.com>
2024-01-26 11:43:16 +0100

    WaitForAsync

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -10,18 +10,25 @@ import {setupCourses} from '../common/setup-test-data';
 
 
 describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync (() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule]
+    })
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(CoursesCardListComponent);
+      component = fixture.componentInstance;
+      // el = fixture.debugElement;
     });
-    });
-  });
+  }) );
 
 
   it("should create the component", () => {
-
-   pending();
+    expect(component).toBeTruthy();
+    console.log("!!!component", component);
 
   });
 
