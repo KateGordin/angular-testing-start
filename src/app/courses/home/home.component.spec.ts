6c0139c4252b51b46bbd367963086ef8ccfc8c8b
KateGordin <kate.gordin@gmail.com>
2024-02-01 15:09:17 +0100

    Add tests

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -22,6 +22,7 @@ describe('HomeComponent', () => {
   let coursesService: any;
 
   const beginnerCourses = setupCourses().filter(course => course.category === 'BEGINNER');
+  const advancedCourses = setupCourses().filter(course => course.category === 'ADVANCED');
 
   beforeEach(waitForAsync(() => {
 
@@ -61,18 +62,24 @@ describe('HomeComponent', () => {
   });
 
 
-  // it("should display only advanced courses", () => {
+  it("should display only advanced courses", () => {
 
-  //     pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css('.mdc-tab'));
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
 
-  // });
+  });
 
 
-  // it("should display both tabs", () => {
+  it("should display both tabs", () => {
 
-  //   pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css('.mdc-tab'));
+    expect(tabs.length).toBe(2, 'Expected to find two tabs');
 
-  // });
+  });
 
 
   // it("should display advanced courses when tab clicked", () => {
