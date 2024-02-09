eb2142741f228c3c34bf9803aaf33077e3f0404d
KateGordin <kate.gordin@gmail.com>
2024-02-09 09:54:13 +0100

    Code with FakeAsync

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, flush, flushMicrotasks } from "@angular/core/testing";
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
 
 fdescribe("Async testing examples", () => {
 
@@ -37,7 +37,7 @@ fdescribe("Async testing examples", () => {
   }))
 
 
-  fit('Asynchronous test example - plain Promise', fakeAsync(() => {
+  it('Asynchronous test example - plain Promise', fakeAsync(() => {
       
       let test = false;
   
@@ -61,6 +61,31 @@ fdescribe("Async testing examples", () => {
       console.log('Running test assertions');
       expect(test).toBeTruthy();
 
-  }))
+  }));
+
+
+
+  it('Asynchronous test example - Promises + setTimeout()', fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+
+    });
+
+    expect(counter).toBe(0);
+    flushMicrotasks();
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(11);
+
+  }));
 
 });
